acd883f90db358516ca74512c0511b3705bdacfe
sdm_seven <793652351@qq.com>
2018-05-14 23:12:00 +0800

    update

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,30 +1,86 @@
-page({
-  data:function(){
-    list:[]
+// pages/list/list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: []
   },
-  onReady: function() {
-   
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  onShow:function(){
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
     var that = this;
     wx.request({
-      url: 'http://127.0.0.1:8082/demo/superadmin/areaList',
-      method:'GET',
-      data:{},
-      success:function(res){
+      url: 'http://127.0.0.1:8082/demo/superadmin/listarea',
+      method: 'GET',
+      data: {},
+      success: function (res) {
+        console.log(res);
         var list = res.data.areaList;
-        if (list == null){
+        if (list == null) {
           wx.showToast({
             title: 'toastTest',
-            icon:'',
-            duration:2000
+            icon: '',
+            duration: 2000
           });
-        }else{
+        } else {
           this.setData({
-            list:list
-          })
+            list: list
+          });
         }
       }
-    })
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
-});
+})
